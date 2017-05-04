84ec5336128b3a0e719322e10776a447b249156d
guyfrominterwebs <aaa>
2017-05-04 11:44:56 +0200

    asd

--- servers/webserver/server/content/cache/0a/0a0f5422378e4d24fe364b69bc48dca72086222980c666f8994079f83e7c79b9.php
+++ servers/webserver/server/content/cache/0a/0a0f5422378e4d24fe364b69bc48dca72086222980c666f8994079f83e7c79b9.php
@@ -14,8 +14,8 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     protected function doGetParent(array $context)
     {
-        // line 5
-        return $this->loadTemplate($this->getAttribute(($context["page"] ?? null), "template", array()), "content.twig", 5);
+        // line 1
+        return $this->loadTemplate($this->getAttribute(($context["page"] ?? null), "template", array()), "content.twig", 1);
     }
 
     protected function doDisplay(array $context, array $blocks = array())
@@ -23,12 +23,12 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
         $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
     }
 
-    // line 6
+    // line 2
     public function block_view($context, array $blocks = array())
     {
-        // line 7
+        // line 3
         echo "\t";
-        $this->loadTemplate("content.twig", "content.twig", 7, "58930913")->display($context);
+        $this->loadTemplate("content.twig", "content.twig", 3, "1373012555")->display($context);
     }
 
     public function getTemplateName()
@@ -43,7 +43,7 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     public function getDebugInfo()
     {
-        return array (  30 => 7,  27 => 6,  18 => 5,);
+        return array (  30 => 3,  27 => 2,  18 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -56,24 +56,24 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     public function getSourceContext()
     {
-        return new Twig_Source("{#
-\tpage - Page
-\tpage.settings
-#}
-{% extends page.template %}
+        return new Twig_Source("{% extends page.template %}
 {% block view %}
 \t{% embed page.layout %}
 \t\t{% block view_left_panel %}
 \t\t\t{% include 'left_bar.twig' %}
 \t\t{% endblock view_left_panel %}
 \t\t{% block view_content %}
-\t\t\t<h1>Here be device info.</h1>
-\t\t\t<div id=\"rt-feed\" class=\"{{ (page.settings ['show'] != 'rt' ? 'hidden') }}\">
-\t\t\t\tIs time real?
+\t\t\t<p>Here be device info.</p>
+\t\t\t{% set show_rt = '' %}
+\t\t\t{% set show_history = 'hidden' %}
+\t\t\t{% if page.settings ['show'] is not empty %}
+\t\t\t\t{% set show_rt = (page.settings ['show'] != 'rt' ? 'hidden') %}
+\t\t\t\t{% set show_history = (page.settings ['show'] != 'history' ? 'hidden') %}
+\t\t\t{% endif %}
+\t\t\t<div id=\"rt-feed\" class=\"{{ show_rt }}\">
 \t\t\t\t{{ dump (data) }}
 \t\t\t</div>
-\t\t\t<div id=\"history-feed\" class=\"{{ (page.settings ['show'] != 'history' ? 'hidden') }}\">
-\t\t\t\tWho writes history?
+\t\t\t<div id=\"history-feed\" class=\"{{ show_history }}\">
 \t\t\t\t{{ dump (data) }}
 \t\t\t</div>
 \t\t{% endblock view_content %}
@@ -85,7 +85,7 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
 
 /* content.twig */
-class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961339a9_58930913 extends Twig_Template
+class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961339a9_1373012555 extends Twig_Template
 {
     public function __construct(Twig_Environment $env)
     {
@@ -99,7 +99,7 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     protected function doGetParent(array $context)
     {
-        return $this->loadTemplate($this->getAttribute(($context["page"] ?? null), "layout", array()), "content.twig", 7);
+        return $this->loadTemplate($this->getAttribute(($context["page"] ?? null), "layout", array()), "content.twig", 3);
     }
 
     protected function doDisplay(array $context, array $blocks = array())
@@ -107,36 +107,52 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
         $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
     }
 
-    // line 8
+    // line 4
     public function block_view_left_panel($context, array $blocks = array())
     {
-        // line 9
+        // line 5
         echo "\t\t\t";
-        $this->loadTemplate("left_bar.twig", "content.twig", 9)->display($context);
-        // line 10
+        $this->loadTemplate("left_bar.twig", "content.twig", 5)->display($context);
+        // line 6
         echo "\t\t";
     }
 
-    // line 11
+    // line 7
     public function block_view_content($context, array $blocks = array())
     {
-        // line 12
-        echo "\t\t\t<h1>Here be device info.</h1>
-\t\t\t<div id=\"rt-feed\" class=\"";
-        // line 13
-        echo ((($this->getAttribute($this->getAttribute(($context["page"] ?? null), "settings", array()), "show", array(), "array") != "rt")) ? ("hidden") : (""));
-        echo "\">
-\t\t\t\tIs time real?
-\t\t\t\t";
+        // line 8
+        echo "\t\t\t<p>Here be device info.</p>
+\t\t\t";
+        // line 9
+        $context["show_rt"] = "";
+        // line 10
+        echo "\t\t\t";
+        $context["show_history"] = "hidden";
+        // line 11
+        echo "\t\t\t";
+        if ( !twig_test_empty($this->getAttribute($this->getAttribute(($context["page"] ?? null), "settings", array()), "show", array(), "array"))) {
+            // line 12
+            echo "\t\t\t\t";
+            $context["show_rt"] = ((($this->getAttribute($this->getAttribute(($context["page"] ?? null), "settings", array()), "show", array(), "array") != "rt")) ? ("hidden") : (""));
+            // line 13
+            echo "\t\t\t\t";
+            $context["show_history"] = ((($this->getAttribute($this->getAttribute(($context["page"] ?? null), "settings", array()), "show", array(), "array") != "history")) ? ("hidden") : (""));
+            // line 14
+            echo "\t\t\t";
+        }
         // line 15
+        echo "\t\t\t<div id=\"rt-feed\" class=\"";
+        echo twig_escape_filter($this->env, ($context["show_rt"] ?? null), "html", null, true);
+        echo "\">
+\t\t\t\t";
+        // line 16
         echo twig_escape_filter($this->env, twig_var_dump($this->env, $context, ($context["data"] ?? null)), "html", null, true);
         echo "
 \t\t\t</div>
 \t\t\t<div id=\"history-feed\" class=\"";
-        // line 17
-        echo ((($this->getAttribute($this->getAttribute(($context["page"] ?? null), "settings", array()), "show", array(), "array") != "history")) ? ("hidden") : (""));
+        // line 18
+        echo twig_escape_filter($this->env, ($context["show_history"] ?? null), "html", null, true);
         echo "\">
-\t\t\t\tWho writes history?
 \t\t\t\t";
         // line 19
         echo twig_escape_filter($this->env, twig_var_dump($this->env, $context, ($context["data"] ?? null)), "html", null, true);
@@ -157,7 +173,7 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     public function getDebugInfo()
     {
-        return array (  142 => 19,  137 => 17,  132 => 15,  127 => 13,  124 => 12,  121 => 11,  117 => 10,  114 => 9,  111 => 8,  30 => 7,  27 => 6,  18 => 5,);
+        return array (  158 => 19,  154 => 18,  149 => 16,  144 => 15,  141 => 14,  138 => 13,  135 => 12,  132 => 11,  129 => 10,  127 => 9,  124 => 8,  121 => 7,  117 => 6,  114 => 5,  111 => 4,  30 => 3,  27 => 2,  18 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -170,24 +186,24 @@ class __TwigTemplate_37f5593376db18f9851d49146aed039e7ef6668c28c89cd33736d800961
 
     public function getSourceContext()
     {
-        return new Twig_Source("{#
-\tpage - Page
-\tpage.settings
-#}
-{% extends page.template %}
+        return new Twig_Source("{% extends page.template %}
 {% block view %}
 \t{% embed page.layout %}
 \t\t{% block view_left_panel %}
 \t\t\t{% include 'left_bar.twig' %}
 \t\t{% endblock view_left_panel %}
 \t\t{% block view_content %}
-\t\t\t<h1>Here be device info.</h1>
-\t\t\t<div id=\"rt-feed\" class=\"{{ (page.settings ['show'] != 'rt' ? 'hidden') }}\">
-\t\t\t\tIs time real?
+\t\t\t<p>Here be device info.</p>
+\t\t\t{% set show_rt = '' %}
+\t\t\t{% set show_history = 'hidden' %}
+\t\t\t{% if page.settings ['show'] is not empty %}
+\t\t\t\t{% set show_rt = (page.settings ['show'] != 'rt' ? 'hidden') %}
+\t\t\t\t{% set show_history = (page.settings ['show'] != 'history' ? 'hidden') %}
+\t\t\t{% endif %}
+\t\t\t<div id=\"rt-feed\" class=\"{{ show_rt }}\">
 \t\t\t\t{{ dump (data) }}
 \t\t\t</div>
-\t\t\t<div id=\"history-feed\" class=\"{{ (page.settings ['show'] != 'history' ? 'hidden') }}\">
-\t\t\t\tWho writes history?
+\t\t\t<div id=\"history-feed\" class=\"{{ show_history }}\">
 \t\t\t\t{{ dump (data) }}
 \t\t\t</div>
 \t\t{% endblock view_content %}
