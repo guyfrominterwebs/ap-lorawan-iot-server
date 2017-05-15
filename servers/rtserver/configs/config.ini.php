fb968e43d819739af9786a35e378495ac3c2e3f2
guyfrominterwebs <aaa>
2017-05-15 13:18:45 +0200

    Added missing mqtt and rt servers. Had been considered as broken submodules upto this point.

---
+++ servers/rtserver/configs/config.ini.php
@@ -0,0 +1,16 @@
+; <?php @ob_end_clean (); die (); ?>
+
+;
+; Legend; indicator part will be removed.
+;	- path				Can be found with path -method using correct section and variable name.
+;	- extension			Can be found with ext -method using correct section and variable name.
+;
+
+[system]
+debug					=true
+
+[server]
+path_root				=.
+path_frameworks			=../frameworks
+path_api				=../server/api
+path_content			=../server/content
